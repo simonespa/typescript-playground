ff5fd5fa78d86ac5caee63e9802303f07db20faf
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2021-08-31 16:33:10 +0100

    Update Jest adding coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,17 @@
 export default {
   roots: [
-    "<rootDir>/test"
+    '<rootDir>/test'
   ],
   testMatch: [
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    '**/?(*.)+(spec|test).+(ts|tsx)'
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  collectCoverage: true,
+  coverageDirectory: '.coverage',
+  coverageProvider: 'v8',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['text', 'lcov'],
+  cacheDirectory: '.jest_cache'
 }
